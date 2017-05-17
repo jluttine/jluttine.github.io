5abe56c5ecd431453b72bd88d38f2173dcdbda9e
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2017-05-17 23:52:09 +0300

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+let
+
+  python = pkgs.python35Full;
+
+  pythonPackages = pkgs.python35Packages;
+
+in {
+  blogEnv = stdenv.mkDerivation {
+    name = "blog";
+    buildInputs = with pythonPackages; [ Nikola python jupyter ws4py watchdog webassets ];
+  };
+}
